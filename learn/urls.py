1d55ed6b5f4caf9d43541af7c79002332f2e7a26
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-06-29 02:12:53 +0300

    Finished URL Mapping for all subject notes and tests.

--- learn/urls.py
+++ learn/urls.py
@@ -6,6 +6,16 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('tests/', views.TestsView.as_view(), name='tests'),
     path('notes/', views.NotesView.as_view(), name='notes'),
-    path('resources/', views.ResourcesView.as_view(), name='resources'),
-
+    path('tests/mathematics/', views.MathTestsView.as_view(), name='math_tests'),
+    path('tests/science/', views.ScienceTestsView.as_view(), name='science_tests'),
+    path('tests/english/', views.EnglishTestsView.as_view(), name='english_tests'),
+    path('tests/kiswahili/', views.KiswahiliTestsView.as_view(), name='kiswahili_tests'),
+    path('tests/social/', views.SocialTestsView.as_view(), name='social_tests'),
+    path('tests/religion/', views.ReligionTestsView.as_view(), name='religion_tests'),
+    path('notes/mathematics/', views.MathNotesView.as_view(), name='math_notes'),
+    path('notes/science/', views.ScienceNotesView.as_view(), name='science_notes'),
+    path('notes/english/', views.EnglishNotesView.as_view(), name='english_notes'),
+    path('notes/kiswahili/', views.KiswahiliNotesView.as_view(), name='kiswahili_notes'),
+    path('notes/social/', views.SocialTestNotes.as_view(), name='social_notes'),
+    path('notes/religion/', views.ReligionNotesView.as_view(), name='religion_notes'),
 ]
--- learn/views.py
+++ learn/views.py
@@ -27,13 +27,101 @@ class NotesView(generic.ListView):
     
     def get_queryset(self):
         return Subject.objects.all()
-    
 
-class ResourcesView(generic.ListView):
-    template_name = 'learn/resources.html'
+
+# Tests
+class MathTestsView(generic.ListView):
+    template_name = 'learn/math_tests.html'
     context_object_name = 'subject_list'
     
     def get_queryset(self):
         return Subject.objects.all()
 
 
+class ScienceTestsView(generic.ListView):
+    template_name = 'learn/science_tests.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("Science")
+
+
+class EnglishTestsView(generic.ListView):
+    template_name = 'learn/english_tests.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("English")
+
+
+class KiswahiliTestsView(generic.ListView):
+    template_name = 'learn/kiswahili_tests.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("Kiswahili")
+
+
+class SocialTestsView(generic.ListView):
+    template_name = 'learn/social_tests.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("Social")
+
+
+class ReligionTestsView(generic.ListView):
+    template_name = 'learn/religion_tests.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("Religion")
+
+
+# Notes
+class MathNotesView(generic.ListView):
+    template_name = 'learn/math_notes.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("Math")
+
+
+class ScienceNotesView(generic.ListView):
+    template_name = 'learn/science_notes.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("Science")
+
+
+class EnglishNotesView(generic.ListView):
+    template_name = 'learn/english_notes.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("English")
+
+
+class KiswahiliNotesView(generic.ListView):
+    template_name = 'learn/kiswahili_notes.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("Kiswahili")
+
+
+class SocialTestNotes(generic.ListView):
+    template_name = 'learn/social_notes.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("Social")
+
+
+class ReligionNotesView(generic.ListView):
+    template_name = 'learn/religion_notes.html'
+    context_object_name = 'subject_list'
+    
+    def get_queryset(self):
+        return HttpResponse("Religion")
