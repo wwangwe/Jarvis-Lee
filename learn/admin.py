cab943c2f0fdcfac48e527f2d7ba7011c61a372e
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-10 02:10:43 +0300

    Use 'inlines' and 'fieldsets' to customize admin panel during reqistration

--- learn/admin.py
+++ learn/admin.py
@@ -1,23 +1,44 @@
 from django.contrib import admin
-from .models import Subject, Test, Question, Answer
+from .models import Subject, Question, Choice, Exam
+
 
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'symbol')
+    fields = ['name']
 
 admin.site.register(Subject, SubjectAdmin)
 
-class TestAdmin(admin.ModelAdmin):
-    list_display = ('test_number','subject', 'max_score')
 
-admin.site.register(Test, TestAdmin)
+class ChoiceAdmin(admin.ModelAdmin):
+    fields = ['choice_text', 'is_correct']
+
+admin.site.register(Choice, ChoiceAdmin)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('test', 'question_text')
+    list_display = ('exam', 'subject', 'max_score', 'question_text')
+    list_filter = ['subject']
+    search_fields = ['question_text']
+    fieldsets = [
+        ('Question Information', {'fields': ['exam', 'subject', 'max_score']}),
+        ('Question', {'fields': ['question_text']})
+    ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 
-class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('question', 'answer_text', 'is_correct')
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 49
 
-admin.site.register(Answer, AnswerAdmin)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject')
+    list_filter = ['subject']
+    fieldsets = [
+        ('Exam Information', {'fields':['name', 'subject']})
+    ]
+    inlines = [QuestionInline]
 
+admin.site.register(Exam, ExamAdmin)
