9aee62c5e30310be5683d48dca0703287c6850cd
Timothy Wangwe <wangwetymothy@gmail.com>
2021-01-01 21:47:04 +0300

    New project structure

--- jarvis/urls.py
+++ jarvis/urls.py
@@ -1,7 +1,11 @@
+"""
+jarvis URL Configuration
+"""
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('learn.urls')),
+    # path('quiz/', include('quizzes.urls')),
+    path('nested_admin/', include('nested_admin.urls')),
 ]
